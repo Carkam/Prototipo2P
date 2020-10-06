1581309a465367f5a4047532bd203ee997dc238d
Carkam <caanco@hotmail.es>
2020-10-06 10:47:25 -0600

    Segundo Parcial

---
+++ Prototipo2P/Prototipo2P/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaVista;
+namespace Prototipo2P
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ingresarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           IngresarProducto ingresarProducto = new IngresarProducto();
+            ingresarProducto.MdiParent = this;
+            ingresarProducto.Show();
+        }
+
+        private void eliminarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EliminarProducto eliminarProducto = new EliminarProducto();
+            eliminarProducto.MdiParent = this;
+            eliminarProducto.Show();
+        }
+    }
+}
